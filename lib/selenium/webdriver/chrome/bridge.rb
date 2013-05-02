5c0e949fc5d5cc7b63194a79c9830763020288c4
Zsolt Sz. Sztupák <zsolt.sztupak@gamesys.co.uk>
2013-05-02 15:15:31 +0100

    added an option to not include the detach option for Chrome

--- lib/selenium/webdriver/chrome/bridge.rb
+++ lib/selenium/webdriver/chrome/bridge.rb
@@ -87,7 +87,7 @@ module Selenium
           chrome_options['binary']                   = Chrome.path if Chrome.path
           chrome_options['nativeEvents']             = true if native_events
           chrome_options['verbose']                  = true if verbose
-          chrome_options['detach']                   = detach.nil? || !!detach
+          chrome_options['detach']                   = detach.nil? || !!detach  unless detach == :unspecified
           chrome_options['noWebsiteTestingDefaults'] = true if no_website_testing_defaults
 
           caps['chromeOptions'] = chrome_options
